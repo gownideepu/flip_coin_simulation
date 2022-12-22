5d93429a2c012b69ce507212f66432ca6a53a06c
unknown <gownideepu23@gmail.com>
2022-12-22 21:07:34 +0530

    number of times the tain and head won

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -6,3 +6,26 @@ then
 else 
 	echo "Tails"
 fi
+
+
+
+A=0
+B=0
+day=0
+while [ $day -lt 21 ]
+do
+	result1=$((RANDOM %2))
+	if [[ $result1 -eq 1 ]];
+      then
+		echo "Heads"
+		((A++))
+	else
+		echo "tails"
+		((B++))
+	fi
+	((day++))
+done
+	echo " the no of time the head won is $A " ;
+	echo " the no of time the tail won is $B " ;
+
+ 
